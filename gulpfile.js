131c127de67a651be0c2b647716f95777f088bb2
Roman Petrashkevich <roman.petrashkevich@skdo.systems>
2022-06-21 19:48:17 +0300

    update templete

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,33 @@
+// Imports
+import gulp from 'gulp';
+import browsersync from 'browser-sync';
+import fileinclude from 'gulp-file-include';
+import del from 'del';
+import dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+import autoprefixer from 'gulp-autoprefixer';
+import groupmedia from 'gulp-group-css-media-queries';
+import cleanCSS from 'gulp-clean-css';
+import rename from 'gulp-rename';
+import uglify from 'gulp-uglify-es';
+import babel from 'gulp-babel';
+import imagemin from 'gulp-imagemin';
+import webp from 'gulp-webp';
+import concat from 'gulp-concat';
+import ttf2woff2 from 'gulp-ttf2woff2';
+import ttf2woff from 'gulp-ttf2woff';
+
+const scss = gulpSass(dartSass)
+const server = browsersync.create();
+
 // PATHs
 const dist_folder = 'production';
 const source_folder = 'app';
 const build_folder = 'gulp-build';
 
-// Imports
-const { src, dest } = require('gulp'),
-  gulp = require('gulp'),
-  browsersync = require('browser-sync').create(),
-  fileinclude = require('gulp-file-include'),
-  del = require('del'),
-  scss = require('gulp-sass'),
-  autoprefixer = require('gulp-autoprefixer'),
-  groupmedia = require('gulp-group-css-media-queries'),
-  cleanCSS = require('gulp-clean-css'),
-  rename = require('gulp-rename'),
-  uglify = require('gulp-uglify-es').default,
-  babel = require('gulp-babel'),
-  imagemin = require('gulp-imagemin'),
-  webp = require('gulp-webp'),
-  concat = require('gulp-concat'),
-  ttf2woff2 = require('gulp-ttf2woff2'),
-  ttf2woff = require('gulp-ttf2woff');
-
 // HTML
 const html = () => {
-  return src([
+  return gulp.src([
     source_folder + '/src/html/*.html',
     '!' + source_folder + '/src/html/_*.html',
   ])
@@ -34,17 +37,17 @@ const html = () => {
         basename: 'index',
       })
     )
-    .pipe(dest(`./app/${build_folder}/`))
-    .pipe(browsersync.stream());
+    .pipe(gulp.dest(`./app/${build_folder}/`))
+    .pipe(server.stream());
 };
 
 const exportHTML = () => {
-  return src(source_folder + `/${build_folder}/index.html`).pipe(dest(dist_folder + '/'));
+  return gulp.src(source_folder + `/${build_folder}/index.html`).pipe(gulp.dest(dist_folder + '/'));
 };
 
 // CSS
 const css = () => {
-  return src(source_folder + '/src/scss/main.scss')
+  return gulp.src(source_folder + '/src/scss/main.scss')
     .pipe(
       scss({
         outputStyle: 'expanded',
@@ -58,7 +61,7 @@ const css = () => {
       })
     )
     .pipe(groupmedia())
-    .pipe(dest(`./app/${build_folder}/css/`))
+    .pipe(gulp.dest(`./app/${build_folder}/css/`))
     .pipe(cleanCSS())
     .pipe(
       rename({
@@ -66,17 +69,17 @@ const css = () => {
         extname: '.min.css',
       })
     )
-    .pipe(dest(`./app/${build_folder}/css/`))
-    .pipe(browsersync.stream());
+    .pipe(gulp.dest(`./app/${build_folder}/css/`))
+    .pipe(server.stream());
 };
 
 const exportCSS = () => {
-  return src(source_folder + `/${build_folder}/css/**/*`).pipe(dest(dist_folder + '/css/'));
+  return gulp.src(source_folder + `/${build_folder}/css/**/*`).pipe(gulp.dest(dist_folder + '/css/'));
 };
 
 // JS
 const js = () => {
-  return src([
+  return gulp.src([
     // add js libs
     // ...
     source_folder + '/src/js/common.js', // Always at the end
@@ -87,39 +90,38 @@ const js = () => {
         presets: ['@babel/preset-env'],
       })
     )
-    .pipe(uglify())
-    .pipe(dest(`./app/${build_folder}/js/`))
-    .pipe(browsersync.stream());
+    .pipe(uglify.default())
+    .pipe(gulp.dest(`./app/${build_folder}/js/`))
+    .pipe(server.stream());
 };
 
 const exportJS = () => {
-  return src(source_folder + `/${build_folder}/js/scripts.min.js`).pipe(
-    dest(dist_folder + `/${build_folder}/js/`)
+  return gulp.src(source_folder + `/${build_folder}/js/scripts.min.js`).pipe(
+    gulp.dest(dist_folder + `/${build_folder}/js/`)
   );
 };
 
 // FONTS
 // TASK, 'gulp convertFonts' to run
-const convertFonts = () => {
-  return src([source_folder + '/src/fonts/**/*.ttf'])
+export const convertFonts = () => {
+  return gulp.src([source_folder + '/src/fonts/**/*.ttf'])
     .pipe(ttf2woff())
-    .pipe(dest(source_folder + '/src/fonts/'))
-    .pipe(src([source_folder + '/src/fonts/**/*.ttf']))
+    .pipe(gulp.dest(source_folder + '/src/fonts/'))
+    .pipe(gulp.src([source_folder + '/src/fonts/**/*.ttf']))
     .pipe(ttf2woff2())
-    .pipe(dest(source_folder + '/src/fonts/'));
+    .pipe(gulp.dest(source_folder + '/src/fonts/'));
 };
-exports.convertFonts = convertFonts;
 
 const exportFont = () => {
-  return src(source_folder + '/src/fonts/**/*').pipe(
-    dest(dist_folder + '/fonts/')
+  return gulp.src(source_folder + '/src/fonts/**/*').pipe(
+    gulp.dest(dist_folder + '/fonts/')
   );
 };
 
 // IMAGES
 // TASK, 'gulp convertImages' to run
-const convertImages = () => {
-  return src([
+export const convertImages = () => {
+  return gulp.src([
     source_folder + '/src/img/**/*',
     '!' + source_folder + '/src/img/favicon/**/*',
   ])
@@ -128,12 +130,11 @@ const convertImages = () => {
         quality: 70,
       })
     )
-    .pipe(dest(source_folder + '/src/img'));
+    .pipe(gulp.dest(source_folder + '/src/img'));
 };
-exports.convertImages = convertImages;
 
 const exportImages = () => {
-  return src(source_folder + '/src/img/**/*')
+  return gulp.src(source_folder + '/src/img/**/*')
     .pipe(
       imagemin({
         progressive: true,
@@ -142,12 +143,12 @@ const exportImages = () => {
         optimizationLevel: 3, // 0 to 7
       })
     )
-    .pipe(dest(dist_folder + '/img'));
+    .pipe(gulp.dest(dist_folder + '/img'));
 };
 
 // Sync
 const browserSync = () => {
-  browsersync.init({
+  server.init({
     server: {
       baseDir: './' + source_folder + `/${build_folder}/`,
     },
@@ -170,7 +171,7 @@ const clean = () => {
 };
 
 // Build project
-const build = gulp.series(
+export const build = gulp.series(
   clean,
   html,
   exportHTML,
@@ -181,10 +182,9 @@ const build = gulp.series(
   exportFont,
   exportImages
 );
-exports.build = build;
 
 // Default
-exports.default = gulp.series(
+export default gulp.series(
   gulp.parallel(js, css, html),
   gulp.parallel(watchFiles, browserSync)
 );
